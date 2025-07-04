ad00cfb81280e88fdb566241168afbcbe782e710
ybaghiyev <ybaghiyev@IT_DEP_10>
2025-07-04 12:06:19 +0400

    updating

--- Shared.Infrastructure/IImageService.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Shared.Infrastructure
-{
-    public interface IImageService
-    {
-        Task<string> UploadImageAsync(Stream imageStream, string fileName, int inventoryCode);
-        Task DeleteImageAsync(string imageUrl);
-        bool IsValidImage(string fileName);
-    }
-}
